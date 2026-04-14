7725a40bb373a3fb4808b4a59746d9bfc3cc1d5d
nikolozi916 <mushkudiani.niko75@gmail.com>
2026-04-14 21:16:21 +0400

    commit

---
+++ LEVEL20/homework/hw20.py
@@ -0,0 +1,38 @@
+# while loop-გამოიყენება როცა გინდა რაღაც მოქმედება განმეორდეს მანამ სანამ მართალი არ იქნება ხოლო for loop-გვეხმარება კოდის გამეორებაში.
+
+password = "GOA BEST 123"
+user_input = input ("Enter password: ")
+
+
+
+while password  != user_input:
+    user_input = input ("Enter password: ")
+    print ("Wrong password")
+
+
+
+
+print ("Correct password")
+
+
+
+
+i = 1
+
+while i <= 10:
+
+    print (i)
+
+    i +=1
+
+
+
+
+i = 2
+
+while i <= 20:
+    print (i)
+    i +=2
+
+
+# for loop-გამოიყენება კოდის გამეორებაში ხოლო while loop-ი გამოიყენება როცა გინდა რაღაც მოქმედება შესრულდეს მანამ სანამ მართალი არ იქნება.
