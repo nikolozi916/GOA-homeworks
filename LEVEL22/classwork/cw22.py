5e8c3c567fbc396f9d93487edcd80e150ef38cee
nikolozi916 <mushkudiani.niko75@gmail.com>
2026-04-19 10:30:36 +0400

    commit

---
+++ LEVEL22/classwork/cw22.py
@@ -0,0 +1,27 @@
+password = "GOA"
+user_input = input ("Enter password: ")
+
+
+while password != user_input:
+    user_input = input ("Enter password: ")
+    print ("Wrong passsword")
+
+
+
+print ("Correct password")
+
+
+
+
+i = 100
+
+while i >= 0:
+    print (i)
+    i -=2
+
+
+for i in range("20,-1,-1"):
+    print ("5")
+
+
+
---
+++ LEVEL22/homework/hw22.py
@@ -0,0 +1,33 @@
+i = 1
+
+while i <= 10:
+    print (i)
+    i +=1
+
+
+
+
+
+i = 30
+
+while i >= 1:
+    print (i)
+    i -=1
+
+
+
+
+
+i = 1
+
+while i <= 100:
+    print (i)
+    i +=2
+
+
+
+i = 3
+
+while i <= 50:
+    print (i)
+    i += 3 
