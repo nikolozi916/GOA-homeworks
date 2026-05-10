44cb1c5b80daeca11565f6ee278744a9f00c7f25
nikolozi916 <mushkudiani.niko75@gmail.com>
2026-05-10 21:32:35 +0400

    commit

---
+++ LEVEL28/classwork/cw28.py
@@ -0,0 +1,18 @@
+product_price = 50 
+user_money = float(input("Enter how much money you have: ")) 
+
+if user_money >= product_price:
+    print("You can buy this product")
+else:
+    print("Not enough money")
+
+
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
---
+++ LEVEL28/homework/hw28.py
@@ -0,0 +1,25 @@
+# 1)
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("you are adult.")
+else:
+    print("you are minor.")
+
+
+
+
+# 2)
+word = input("Enter any word: ")
+
+for char in word:
+    print(char)
+
+
+
+
+# 3)
+for i in range(1, 16):
+    if i % 3 == 0:
+        print(i)
+
